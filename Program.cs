b78d0cb2ba59f3d7341c1458af3a5042ee234e02
Britnay Gore <britnaygore268@gmail.com>
2024-07-09 21:12:38 -0400

    Added Plant data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+List<Plant> plants = new List<Plant>()
+{
+    new Plant()
+    {
+        Species = "Monstera Deliciosa",
+        LightNeeds = 3,
+        AskingPrice = 29.99,
+        City = "Seattle",
+        ZIP = "98109",
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species = "Epipremnum Aureum",
+        LightNeeds = 2,
+        AskingPrice = 14.99,
+        City = "Chicago",
+        ZIP = "60601",
+        Sold = true,
+    },
+    new Plant()
+    {
+        Species = "Sansevieria Trifasciata",
+        LightNeeds = 1,
+        AskingPrice = 19.95,
+        City = "Los Angeles",
+        ZIP = "90001",
+        Sold = false,
+    },
+    new Plant()
+    {
+        Species = "Crassula Ovata",
+        LightNeeds = 5,
+        AskingPrice = 34.50,
+        City = "Miami",
+        ZIP = "33133",
+        Sold = true,
+    },
+    new Plant()
+    {
+        Species = "Ficus Lyrata",
+        LightNeeds = 3,
+        AskingPrice = 59.99,
+        City = "New York",
+        ZIP = "10001",
+        Sold = true,
+    },
+};
